7bc2aa073e548d39ffec4d89e2e4509f5a830392
shanktheenigma <tm0272090@gmail.com>
2025-06-03 22:47:53 +0630

    Api added

---
+++ search-page.js
@@ -0,0 +1,19 @@
+fetch("search-page.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const container = document.getElementById("cardContainer");
+    data.forEach((item) => {
+      const card = document.createElement("div");
+      card.className = "card";
+      card.innerHTML = `
+            <img src="${item.Poster}" alt="${item.Title}" />
+            <h3>${item.Title}</h3>
+            <p>${item.Year}</p>
+            <p><strong>Genre:</strong> ${item.Genre}</p>
+          `;
+      container.appendChild(card);
+    });
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
