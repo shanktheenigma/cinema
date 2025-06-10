3843d38f36885243687edfa6e79516fa85ba82eb
shanktheenigma <tm0272090@gmail.com>
2025-06-10 23:38:46 +0630

    Ngr saye nyid

--- movie.js
+++ movie.js
@@ -4,14 +4,13 @@ fetch("movies.json")
   .then((response) => response.json())
   .then((data) => {
     allMovies = data;
-    displayMovies(data); // Show all movies initially
+    displayMovies(data); // Show all movies
   })
   .catch((error) => {
-    console.error("Error fetching data:", error);
+    console.error("Error fetching data:", error); //output error
   });
 
 const container = document.getElementById("filterList");
-
 // Add "All" button
 const allBtn = document.createElement("button");
 allBtn.className = "filter-button all-button";
@@ -22,25 +21,24 @@ container.appendChild(allBtn);
 for (let i = 65; i <= 90; i++) {
   const btn = document.createElement("button");
   btn.className = "filter-button";
-  btn.textContent = String.fromCharCode(i); // A to Z
+  btn.textContent = String.fromCharCode(i); // A to Z letter
   container.appendChild(btn);
 }
 
 // Attach click event
-container.addEventListener("click", (e) => {
-  if (!e.target.classList.contains("filter-button")) return;
+container.addEventListener("click", (e) => {  
+  if (!e.target.classList.contains("filter-button")) return; //Highlight the clicked button specificially 
 
   [...container.children].forEach((btn) =>
     btn.classList.remove("active")
   );
-  e.target.classList.add("active");
+  e.target.classList.add("active"); 
 
-  const selected = e.target.textContent;
-
-  if (selected === "All") {
-    displayMovies(allMovies);
+  const selected = e.target.textContent; //select the text of the button
+  if (selected === "All") { 
+    displayMovies(allMovies); // display All movie from json
   } else {
-    const filteredMovies = allMovies.filter((movie) =>
+    const filteredMovies = allMovies.filter((movie) => //initalized all movies from the movies.json as movie
       movie.Title.toLowerCase().startsWith(selected.toLowerCase())
     );
     displayMovies(filteredMovies);
@@ -54,9 +52,7 @@ function displayMovies(movies) {
   if (movies.length === 0) {
     cardContainer.innerHTML = "<p>No movies found.</p>";
     return;
-  }
-
-  movies.forEach((item) => {
+  }movies.forEach((item) => {
     const card = document.createElement("div");
     card.className = "card";
     card.innerHTML = `
@@ -68,3 +64,5 @@ function displayMovies(movies) {
     cardContainer.appendChild(card);
   });
 }
+
+
